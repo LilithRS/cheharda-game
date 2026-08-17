e3dec9c9594ab22af62365bd774cd9ce7d5ae75e
LilithRS <t548rysaeva1@gmail.com>
2026-08-18 03:02:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const http = require('http');
 const socketIo = require('socket.io');
 
 const app = express();
+app.use((req, res, next) => {
+  console.log(`Запрос: ${req.method} ${req.url}`);
+  next();
+});
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
